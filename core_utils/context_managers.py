136da55dc0cba8d9779a32acfc561a58d9ed31b7
divynshu <156162995+divynshu@users.noreply.github.com>
2025-12-15 03:00:32 +0530

    Create context_managers.py

---
+++ core_utils/context_managers.py
@@ -0,0 +1,22 @@
+#file manager
+from contextlib import contextmanager
+
+@contextmanager
+def open_file(path, mode):
+    f = open(path, mode)
+    try:
+        yield f
+    finally:
+        f.close()
+
+#timer context context
+
+from contextlib import contextmanager
+
+@contextmanager
+def open_file(path, mode):
+    f = open(path, mode)
+    try:
+        yield f
+    finally:
+        f.close()
